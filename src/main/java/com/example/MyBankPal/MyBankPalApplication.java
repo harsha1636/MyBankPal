1cdd1b8a6f65db25068bfe4d3848ebcc72d63949
harsha1636 <harsha60201@gmail.com>
2025-07-07 22:58:36 +0530

    Initial project setup from Spring Initializr

---
+++ src/main/java/com/example/MyBankPal/MyBankPalApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyBankPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyBankPal/MyBankPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBankPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
